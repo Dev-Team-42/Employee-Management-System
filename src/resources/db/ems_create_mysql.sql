08baecded55d5dd0ced67085231ec9342f6f0784
Amari Booth <amaridb10@gmail.com>
2026-04-25 03:23:50 -0400

    DOB values added to employees table.

--- src/resources/db/ems_create_mysql.sql
+++ src/resources/db/ems_create_mysql.sql
@@ -53,7 +53,7 @@ CREATE TABLE addresses (
 
 CREATE TABLE employees (
     empID INT NOT NULL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE, -- Added username
+    username VARCHAR(50) NOT NULL UNIQUE,
     firstName VARCHAR(50) NOT NULL,
     lastName VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
